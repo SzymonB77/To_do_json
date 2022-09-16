572e016ca90837e21cf74c3ff3400bd5fd1816b3
Szymon Biel <szymek.2k@gmail.com>
2022-09-16 19:14:22 +0200

    fix_add_migrations

--- db/migrate/20220916162904_create_tasks.rb
+++ db/migrate/20220916162904_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :name, null false
+      t.string :name, null: false
       t.string :note
-      t.boolean :is_done, :default: false
+      t.boolean :is_done, default: false
      
       t.timestamps
     end
