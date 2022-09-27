324a5ec28c8fdda9d68c179e6b08e60a23766fc9
Szymon Biel <szymek.2k@gmail.com>
2022-09-27 17:17:58 +0200

    modify_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
-Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+module Api
+  module V1
+    Rails.application.routes.draw do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      namespace :api do
+        namespace :v1 do
+          resources :tasks
+        end
+      end
+      
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
